018a65739382d9d865c5bbcb342c6a118306f189
Rui Fernandes <rui.fernandes@cetelem.pt>
2017-10-02 15:43:06 +0100

    bug 44

--- source/packt/book/jee_eclipse/git/TesteGIT.java
+++ source/packt/book/jee_eclipse/git/TesteGIT.java
@@ -2,6 +2,6 @@ package packt.book.jee_eclipse.git;
 
 public class TesteGIT {
 	public static void main(String[] args) {
-		System.out.println("Helloooooo");
+		System.out.println("Helloooooo from bug fix #44");
 	}
 }
